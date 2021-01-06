6d091ca02db0c5910fc23a3fc3dd8943e399b654
Igor Napierala <igor.napierala@cognifide.com>
2021-01-07 00:47:40 +0100

    draw with canvas

--- src/app.js
+++ src/app.js
@@ -1,10 +1,33 @@
 import { Observable } from "rxjs/Rx";
 
+const canvas = document.getElementById("rxjs-draw-board");
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
+const ctx = canvas.getContext("2d");
+const canvasData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
+
 const output = document.getElementById("output");
 const moveStream$ = Observable.fromEvent(document, "mousemove");
 
+const drawPixel = (x, y, r, g, b, a) => {
+    const index = (x + y * canvasWidth) * 4;
+
+    canvasData.data[index + 0] = r;
+    canvasData.data[index + 1] = g;
+    canvasData.data[index + 2] = b;
+    canvasData.data[index + 3] = a;
+}
+
+const updateCanvas = () => {
+    ctx.putImageData(canvasData, 0, 0);
+}
+
 moveStream$.subscribe(
-  (e ) => (output.innerHTML = `<h1>${e.clientX} ${e.clientY}</h1>`),
+  (e ) => {
+    drawPixel(e.clientX, e.clientY, 255, 0, 0, 255);
+    updateCanvas();
+    output.innerHTML = `<h1>${e.clientX} ${e.clientY}</h1>`;
+    },
   (err) => console.log(err),
   () => console.log("Completed")
 );
